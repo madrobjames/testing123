ee7814f887e1ef8e4a4d5f7309631d3017fcebd8
madrobjames <35857107+madrobjames@users.noreply.github.com>
2018-01-28 23:47:46 +0000

    Add files via upload

---
+++ sdl2.lib/testing2.js
@@ -0,0 +1,18 @@
+﻿package SimpleStocks.Model;
+
+import SimpleStocks.Exceptions.StockTypeNotFoundException;
+
+/**
+ * done by .
+ */
+public interface IStock {
+    String GetStockSymbol();
+StockType GetStockType();
+int GetVariableDividend();
+void SetVariableDividend(int variableDividend);
+double GetFixedDividend();
+void SetFixedDividend(int fixedDividend);
+int GetParValue();
+double CalculateDividendYield(double marketPrice) throws StockTypeNotFoundException;
+double CalculatePERatio(double marketPrice) throws StockTypeNotFoundException;
+}
---
+++ sdl2.lib/testing3.js
@@ -0,0 +1,14 @@
+﻿package SimpleStocks.Model;
+
+import java.util.Date;
+
+/**
+ * done
+ */
+public interface ITrade {
+    IStock GetTradedStock();
+    Date GetTradeTimestamp();
+    int GetTradedQuantity();
+    TradeIndicator GetTradeIndicator();
+    double GetTradedPrice();
+}
